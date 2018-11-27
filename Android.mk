e13f76ef37bc6f102fe46b21500ad3d82f0d0e58
Pavan Katari <43464081+pavankatari@users.noreply.github.com>
2018-11-27 14:03:50 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := DarknessControl
-LOCAL_SRC_FILES := Darkness-Control.apk
+LOCAL_SRC_FILES := DarknessControl.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
